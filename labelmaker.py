2f06f24d3a7393676649e3d74ffd1918e7289b7f
Koen Martens <gmc@sonologic.nl>
2021-09-11 18:48:53 +0200

    Add --printer option, add --font_size option

--- labelmaker.py
+++ labelmaker.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from typing import Optional
 
@@ -14,9 +15,11 @@ from barcode.writer import ImageWriter
                                                   'be printed')
 @click.option('--dpi', type=int, default=200, help='DPI of generated image (default 200)')
 @click.option('--image_size', type=float, default=4, help='Image size in cm (default 4)')
+@click.option('--font_size', type=int, default=25, help='Font size in pt (default 25)')
 @click.option('--module_width', type=float, default=None, help='Force module_width (mm), default is to use the largest'
                                                                'module_width that fits')
-def main(start, end, dpi, image_size, module_width):
+@click.option('--printer', type=str, default="", help="Print each label to printer")
+def main(start, end, dpi, image_size, font_size, module_width, printer):
     if end == -1:
         end = start
 
@@ -25,7 +28,7 @@ def main(start, end, dpi, image_size, module_width):
         click.echo(f'Generating {image_name}')
 
         width, height = _calculate_image_dimensions(dpi, image_size)
-        barcode_image = _render_barcode(width, height, code, dpi, module_width)
+        barcode_image = _render_barcode(width, height, font_size, code, dpi, module_width)
 
         barcode_width, barcode_height = barcode_image.size
 
@@ -35,6 +38,9 @@ def main(start, end, dpi, image_size, module_width):
 
         image.save(image_name)
 
+        if printer:
+            os.system(f'lpr -P "{printer}" "{image_name}"')
+
 
 def _calculate_image_dimensions(dpi, image_size_cm):
     image_size_inch = image_size_cm / 2.54
@@ -42,9 +48,9 @@ def _calculate_image_dimensions(dpi, image_size_cm):
     return image_size_px, image_size_px
 
 
-def _render_barcode(max_width: int, max_height: int, code: int, dpi: int, module_width: Optional[float]):
+def _render_barcode(max_width: int, max_height: int, font_size: int, code: int, dpi: int, module_width: Optional[float]):
     if module_width:
-        image = _render_barcode_with_size(code, dpi, module_width)
+        image = _render_barcode_with_size(code, dpi, font_size, module_width)
         width, height = image.size
         if width <= max_width and height <= max_height:
             return image
@@ -52,7 +58,7 @@ def _render_barcode(max_width: int, max_height: int, code: int, dpi: int, module
         sys.exit(-1)
 
     for size in reversed(sorted(SIZES.keys())):  # try size from big to small until it fits
-        image = _render_barcode_with_size(code, dpi, SIZES[size])
+        image = _render_barcode_with_size(code, dpi, font_size, SIZES[size])
         width, height = image.size
         if width <= max_width and height <= max_height:
             return image
@@ -61,11 +67,11 @@ def _render_barcode(max_width: int, max_height: int, code: int, dpi: int, module
     sys.exit(-1)
 
 
-def _render_barcode_with_size(code: int, dpi: int, module_width: float):
+def _render_barcode_with_size(code: int, dpi: int, font_size: int, module_width: float):
     code_str = f'{code:012}'
     ean = barcode.get('ean13', code_str, writer=ImageWriter())
     ean.writer.dpi = dpi
-    image = ean.render(writer_options={'module_width': module_width}, text=f'{code}')
+    image = ean.render(writer_options={'module_width': module_width, 'font_size': font_size}, text=f'{code}')
     return image
 
 
